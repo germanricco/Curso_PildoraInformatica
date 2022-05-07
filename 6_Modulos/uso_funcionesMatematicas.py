bd97158b4b141fedfe9f4af2b072326495b19eb9
German Ricco <germanricco97@gmail.com>
2022-05-07 20:25:39 -0300

    Modulos y ejercicio de funciones

---
+++ 6_Modulos/uso_funcionesMatematicas.py
@@ -0,0 +1,15 @@
+#import funciones_matematicas
+#Usamos el modulo llamado funciones matematicas
+#funciones_matematicas.sumar(7,5)
+#funciones_matematicas.restar(7,5)
+
+
+#Tambien podemos cambiar la directiva import y traer
+#Unicamente la funcion a utilizar (no tan comodo pero optimiza)
+#from funciones_matematicas import sumar
+#sumar(6,7)
+
+
+#El asterisco sirve para traer todas las funciones del modulo
+from funciones_matematicas import *
+restar(7,2)
---
+++ Metodos_numericos.py
@@ -0,0 +1,6 @@
+nombreUsuario = input("Introduce tu nombre de usuario: ")
+
+print(f"el nombre es: {nombreUsuario}")
+print(f"el nombre es: {nombreUsuario.upper()}")
+print(f"el nombre es: {nombreUsuario.lower()}")
+print(f"el nombre es: {nombreUsuario.capitalize()}")
